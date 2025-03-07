a4b11df0b70c24ebbd56e11331e0bc74b98a9bde
gitahisamuel <murimisamuel05@gmail.com>
2025-03-07 18:05:56 +0300

    Do not push on a friday

---
+++ src/app/worscout/page.tsx
@@ -0,0 +1,14 @@
+import Client from '@/components/Client'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      this is a route
+
+      <Client/>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/components/Client.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Client = () => {
+  return (
+    <div>
+      Hello This Is a  client compomnent
+    </div>
+  )
+}
+
+export default Client
